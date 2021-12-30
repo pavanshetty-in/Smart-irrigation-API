e1679f23167e34ae8cd1e1431089f855a73965c6
Pavan S <pavans.shetty.2452@gmail.com>
2021-12-30 17:24:47 +0530

    working setdata api

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,38 @@
-require('dotenv').config;
+require('dotenv').config({path:"./../.env"});
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const static_path = path.join(__dirname, '../public');
 
 require('./db/conn');
+const Weatherdata= require('./models/weatherdataSchema');
 
 app.use(express.json());
 app.use(express.urlencoded({extend: false}));
 app.use(express.static(static_path));
 
 app.post('/setdata', async (req, res) => {
-    const { date, time, temp, humidity, soilmoist, motorstatus } = req.body;
-    try {
-        const weatherdata = new Weatherdata({
-            date, time, temp, humidity, soilmoist, motorstatus
-        });
-        await weatherdata.save();
+    const {  temp, humidity, soilmoist, motorstatus } = req.body;
+    let date= new Date();
+    if(temp && humidity && soilmoist && motorstatus)
+    {
+        try {
+            const weatherdata = new Weatherdata({
+            date, temp, humidity, soilmoist, motorstatus
+            });
+            await weatherdata.save();
+            console.log("data inseted");
+            res.status(200).send("data sent successfully")
+        }
+
+        catch (err) {
+            console.error(err);
+        }
     }
-    catch (err) {
-        console.error(err);
-    }
-})
+    else{console.log("Values can't be empty")}
+});
 
 app.listen(PORT, () => {
-    console.log('listening on port ${PORT}');
+    console.log(`listening on port ${PORT}`);
 });
--- server/src/models/weatherdataSchema.js
+++ server/src/models/weatherdataSchema.js
@@ -2,6 +2,27 @@ const mongoose = require('mongoose');
 
 const weatherdataSchema = new mongoose.Schema({
     date:{
-        
+        type: Date,
+        required: true,
+    },
+    temp:{
+        type: Number,
+        required: true,
+    },
+    humidity: {
+        type: Number,
+        required: true,
+    },
+    soilmoist: {
+        type: Number,
+        required: true,
+    },
+    motorstatus: {
+        type: Boolean,
+        required: true,
     }
-})
+});
+
+const Weatherdata = mongoose.model('weatherdata',weatherdataSchema);
+
+module.exports= Weatherdata;
