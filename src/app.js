59d8bca3323e79ba24476f34aca26c8e06a27e8e
Pavan S <pavans.shetty.2452@gmail.com>
2022-02-22 18:18:20 +0530

    view index

--- src/app.js
+++ src/app.js
@@ -4,13 +4,45 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, "../templates/views");
+const moment = require("moment");
+
+app.use((req, res, next)=>{
+    res.locals.moment = moment;
+    next();
+  });
 
 require('./db/conn');
 const Weatherdata= require('./models/weatherdataSchema');
 
 app.use(express.json());
 app.use(express.urlencoded({extend: false}));
+app.set("view engine", "ejs");
 app.use(express.static(static_path));
+app.set("views", template_path);
+
+app.get("/", async (req, res) => {
+    try {
+        const Weatherlatest = await Weatherdata.findOne().sort({ date: -1 }).limit(1) ;
+        if (!Weatherlatest) {
+          throw new Error("Services not found");
+        }
+        const Weatherlatest10 = await Weatherdata.find().sort({ date: -1 }).limit(10) ;
+        let temparray=[],datearray=[]
+
+        for(i=0;i<Weatherlatest10.length;i++){
+            temparray.push(Weatherlatest10[i].temp)
+            datearray.push(Weatherlatest10[i].date)
+        }
+        
+        res.render("index",{Weatherlatest:Weatherlatest,temparray:temparray,datearray:datearray });
+    }
+    catch (err) {
+        res.status(401).send("Unauthorized:No token provided");
+        console.log(err);
+    }
+   
+  });
 
 app.post('/se', async (req, res) => {
     const {  temp, humidity, soilmoist, motorstatus } = req.body;
